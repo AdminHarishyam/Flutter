80ffce7aa23f688501c307d2ccc230e0ed9b1dd2
375522 <harishyampkd@gmail.com>
2023-09-06 12:33:19 +0530

    calling a widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:manappuram/widgets/home_widget.dart';
 
-//
-//
-//void main() =>runApp(MaterialApp(home: Text('Hello World'),));
-void main(){
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.cyan,
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.deepPurple,
-        title: Text("app title bar"),
-      ),
-      body:Center(child:Text('Welcome You',
-      style: TextStyle(
-        color: Colors.deepOrange,
-        fontSize: 20.0,
-        fontWeight: FontWeight.bold
-      ),
-      ) ,),
-      floatingActionButton: FloatingActionButton(
-        onPressed: increment(),
-        child: Text('Click'),
-      ),
-      //Text('Ready set Go'),
-    )));
-  //int c= add(10,20);
+void main() {
+ try{
+   runApp(MaterialApp(
+     home: HomeWidget(),
+   ));
+ }catch(e,s){
 
-}
-int add(int a,int b){
-  return a+b;
-}
-increment(){
-
+ }
 }
---
+++ lib/widgets/home_widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class HomeWidget extends StatefulWidget {
+  const HomeWidget({super.key});
+
+  @override
+  State<HomeWidget> createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  int _counter = 0;
+
+  void _incrementCounter(){
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("header app bar title"),
+      ),
+      body: Center(
+          child: Text(
+            "you clicked \n $_counter \n no of  times",
+            style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepOrange
+            ),
+          )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+
+}
