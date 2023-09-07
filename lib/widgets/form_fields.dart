d724f1210c0fa0212c6649f77b783e9c577dc81f
375522 <harishyampkd@gmail.com>
2023-09-07 11:09:49 +0530

    calling a widgets with form fields and onpressed

--- lib/widgets/form_fields.dart
+++ lib/widgets/form_fields.dart
@@ -1,68 +1,62 @@
 import 'package:flutter/material.dart';
-
 void main()=> runApp(MaterialApp(
     home: MyCustomForm()
 ),
 );
-
 class MyCustomForm extends StatefulWidget {
   const MyCustomForm({super.key});
-
   @override
   State<MyCustomForm> createState() {
     return _MyCustomFormState();
   }
 }
-
-
 class _MyCustomFormState extends State<MyCustomForm> {
   final myController = TextEditingController();
-
   @override
   void dispose() {
     myController.dispose();
     super.dispose();
   }
 
-  void showMyDialog(BuildContext context){
-    AlertDialog alertDialog = AlertDialog();
 
-    showDialog(context: context, builder: (context){
-      return AlertDialog(content: Text(myController.text),);
-    });
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          TextField(
-            controller: myController,
-            decoration: InputDecoration(
-                hintText: "enter search word",
-                border: OutlineInputBorder()
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        body: Column(
+          children: [
+            TextField(
+              onChanged: (text){
+                print('First text field: $text (${text.characters.length})');
+              },
+              controller: myController,
+              decoration: InputDecoration(
+                  hintText: "enter search word",
+                  border: OutlineInputBorder()
+              ),
             ),
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: "enter username",
-              border: UnderlineInputBorder(),
-            ),
-          )
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          showDialog(context: context,
-              builder: (context){
-                return AlertDialog(
-                  content: Text(myController.text),
-                );
-              });
-        },
-        child: Icon(Icons.text_fields),
-      ),
-    );
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: "enter username",
+                border: UnderlineInputBorder(),
+              ),
+            )
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            showDialog(context: context,
+                builder: (context){
+                  return AlertDialog(
+                    content: Text(myController.text),
+                  );
+                });
+            showDialog(context: context, builder: (context){
+              return AlertDialog(content: Text(myController.text),);
+            });
+          },
+          child: Icon(Icons.text_fields),
+        ),
+      );
+    }
   }
-}
